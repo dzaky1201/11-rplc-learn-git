cdb1794cd71105b311528fc1e862eab414e65af0
Isham0000 <ishamhidayatullah154@gmail.com>
2021-11-22 09:07:49 +0700

    Update

--- app/src/main/java/com/example/contohprojek/MainActivity.kt
+++ app/src/main/java/com/example/contohprojek/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // ini adalah comment
+        // saya mau nambah fitur pembayaran
+        // senggol dongg
     }
 }
